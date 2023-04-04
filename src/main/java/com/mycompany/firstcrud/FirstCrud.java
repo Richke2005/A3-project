057eee2f88793855add542196d2f3d3007531b7a
Richke2005 <richardke18@gmail.com>
2023-04-04 18:29:56 -0300

    alteração

--- src/main/java/com/mycompany/firstcrud/FirstCrud.java
+++ src/main/java/com/mycompany/firstcrud/FirstCrud.java
@@ -11,6 +11,6 @@ package com.mycompany.firstcrud;
 public class FirstCrud {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Our first Crud");
     }
 }
