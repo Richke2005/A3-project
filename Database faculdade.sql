0793332842cc20c14c6a0401974a2f1abd97830f
Richard Carvalho dos Anjos - 312311297 <312311297@ulife.com.br>
2023-05-25 20:20:23 -0300

    Update database faculdade

--- Database faculdade.sql
+++ Database faculdade.sql
@@ -3,22 +3,25 @@ CREATE DATABASE faculdade;
 USE faculdade;
 
 SHOW TABLES;
-DESCRIBE Student;
-DESCRIBE Adm;
+DESCRIBE student;
+DESCRIBE users;
+DESCRIBE curses
 
-CREATE TABLE Student(
+CREATE TABLE student(
 ra INT AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
 endereco VARCHAR(60),
 celular CHAR(14),
 email VARCHAR(50),
 curso VARCHAR(60),
+curse_key INT NOT NULL, 
+FOREIGN KEY (curse_key) REFERENCES curses(curse_id),
 PRIMARY KEY(ra)
 );
 
 SELECT * FROM Student;
 
-CREATE TABLE Adm(
+CREATE TABLE users(
 cpf CHAR(12) NOT NULL, 
 nome VARCHAR(50) NOT NULL, 
 email VARCHAR(60) NOT NULL, 
@@ -27,4 +30,13 @@ celular CHAR(14),
 PRIMARY KEY(cpf)
 );
 
-SELECT * FROM Adm;
+SELECT * FROM users;
+
+CREATE TABLE curses(
+curse_id INT NOT NULL AUTO_INCREMENT,
+professor VARCHAR(50) NOT NULL,
+descricao VARCHAR (300),
+PRIMARY KEY(curse_id)
+)
+
+SELECT * FROM curses;
