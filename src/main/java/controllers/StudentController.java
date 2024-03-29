ca585c4762a028984a9bde55cc7c7d38faac513a
Richard ´C Dos Anjos <richardke18@gmail.com>
2024-03-29 02:02:55 +0000

    insert of controllers

---
+++ src/main/java/controllers/StudentController.java
@@ -0,0 +1,14 @@
+package controllers;
+import java.util.ArrayList;
+import java.util.List;
+import classes.Student;
+
+public class StudentController {
+    public static List<Student> getAll(){
+
+    }
+
+    public static Student getById(int id){
+
+    }
+}
